ae038f2f90d2a45ca440e6aac6fce0a1651b10d0
Nathan Yin <nathanzjyin@gmail.com>
2023-10-11 17:19:27 +1100

    added hud

--- main.gd
+++ main.gd
@@ -32,9 +32,27 @@ func new_game():
 	$Player.show()
 	$GameTimer.start()
 	spawn_coins()
+	$HUD.update_score(score)
+	$HUD.update_timer(time_left)
 
-
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	get_tree().call_group("coins", "queue_free")	# Remove all notes in group "coins"
+	$HUD.show_game_over()
+	$Player.die()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_game_timer_timeout():
+	time_left -= 1
+	$HUD.update_timer(time_left)
+	if time_left <= 0:
+		game_over()
+
+
+func _on_hud_start_game():
+	new_game()
